8931781f50266f2081a9587a4146bfba29b6fa08
sarangbaraskar <sarangbaraskar07@gmail.com>
2025-11-04 18:13:50 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Todopage />}/>
+        <Route path="/" element={<Todopage/>}/>
       </Routes>
       <ToastContainer />
 
