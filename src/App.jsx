342b5265d0960585e921bc64754607d90e9d03ef
sarangbaraskar <sarangbaraskar07@gmail.com>
2025-11-04 18:21:58 +0530

    import mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+
 import './App.css'
-import Todopage from './pages/todopage'
 import { ToastContainer } from 'react-toastify'
 import { Route, Routes } from 'react-router'
+import Todoform from './component/Todoform'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Todopage/>}/>
+        <Route path="/" element={<Todoform/>}/>
       </Routes>
       <ToastContainer />
 
